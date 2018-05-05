d849464123885e12be944de0221f2411644c4c73
yahyaizhar <yahyaizhar@gmail.com>
2018-05-05 22:17:24 +0500

    test

---
+++ files/js/nomad_data.js
@@ -0,0 +1,11 @@
+$.get( "../files/bin/var/www/html/data/whereamii.html", function( data ) {
+// var href =  $('<div>'+data+'</div>').map(function(index) {
+//     // this callback function will be called once for each matching element
+//     return $(this).find('a').attr('href');
+// });
+var data_compiled = $('<div>'+data+'</div>').find('a:first');
+  var href = $('<div>'+data+'</div>').find('a').attr('href');
+  $(data_compiled).attr('href', 'https://nomadlist.com/petracca'+href);
+  $( ".location" ).html( data_compiled );
+  //console.log( href );
+});
